161dfc610cfd39482866d98532139ce1a79a34c8
joaoxyz <jpreis808@gmail.com>
2023-11-27 15:01:29 -0300

    Save 3

--- model.py
+++ model.py
@@ -4,12 +4,11 @@ from mlxtend.frequent_patterns import apriori, association_rules
 df = pd.read_csv('datasets/2023_spotify_ds1.csv')
 
 # Dataset cleanup
-df['song'] = list(zip(df.artist_name, df.track_name))
-df = df.groupby(['pid', 'song'])['duration_ms'].sum().unstack().reset_index().fillna(0).set_index('pid')
+#df['song'] = list(zip(df.artist_name, df.track_name))
+df = df.groupby(['pid', 'track_name'])['duration_ms'].sum().unstack().reset_index().fillna(0).set_index('pid')
 df = df.applymap(lambda x: True if x > 0 else False)
 
 frequent_itemsets = apriori(df, min_support=0.06, use_colnames=True)
 rules = association_rules(frequent_itemsets, metric="lift", min_threshold=1)
 
-with open("model-results.json", mode="w") as file:
-    file.write(rules.to_json())
+rules.to_pickle('model_results.pickle')
--- request_server.py
+++ request_server.py
@@ -1,14 +1,28 @@
+import pickle
+import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from datetime import date
 
-app = FastAPI()
-
-class Response(BaseModel):
+class Recommendation(BaseModel):
     playlist_ids: List[str]
     version: str
     model_date: str
 
+class ModelInput(BaseModel):
+    songs: List[str]
+
+app = FastAPI()
+
+with open('model_results.pickle', 'rb') as file:
+    recommender = pickle.load(file)
+
 @app.post("/api/recommend")
-def generate_recommendation(resp : Response):
-    pass
+def generate_recommendation(inp : ModelInput) -> Recommendation:
+    result = []
+    for song in inp.songs:
+        mask = [True if song in i else False for i in recommender['antecedents']]
+        result += recommender[mask].explode('consequents')['consequents'].unique()
+    result = list(set(result))
+    return Recommendation(playlist_ids=result, version='1', model_date=date.today().isoformat())
